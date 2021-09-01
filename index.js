c4795191281ea550f4dcc61551b6453e315b8701
Simit Yadav <70283569+Simit-Yadav@users.noreply.github.com>
2021-09-01 02:31:16 +0000

    updating

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.urlencoded({extended: true}))
 app.use(function(req,res,next){
 	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Methods: GET, POST, PUT');
 	next();
 })
 app.use("/api/v1/questions",quiz)
