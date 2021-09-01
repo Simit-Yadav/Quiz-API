0482f20d7f63bb036c178ef0876c98ea7b484baa
Simit Yadav <70283569+Simit-Yadav@users.noreply.github.com>
2021-09-01 02:11:29 +0000

    parsing data update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 
 const quiz = require("./routes/q&a");
@@ -12,7 +13,8 @@ dotenv.config({path: "./keys.env"});
 let PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.urlencoded({extended: true}))
 app.use(function(req,res,next){
 	res.header("Access-Control-Allow-Origin", "*");
 	next();
